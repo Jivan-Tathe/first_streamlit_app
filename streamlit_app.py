5da63a2adfdcdaafb8f6ae8bba7d7eba307aef17
Jivan-Tathe <122883760+Jivan-Tathe@users.noreply.github.com>
2023-01-17 19:43:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.titke('My Parents New Healthy Diner ')
+
+stream.lit.header('Breakfast Menu')
+streamlit.text('omega 3 & Blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
