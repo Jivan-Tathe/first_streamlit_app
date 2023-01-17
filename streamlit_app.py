84ea08a3674e0bb3e15139ecaec115f5175af380
Jivan-Tathe <122883760+Jivan-Tathe@users.noreply.github.com>
2023-01-17 19:54:58 +0530

    Update streamlit_app.py
    added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 streamlit.titke('My Parents New Healthy Diner ')
 
-stream.lit.header('Breakfast Menu')
-streamlit.text('omega 3 & Blueberry oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+stream.lit.header('Breakfast Favorites')
+streamlit.text('🍵omega 3 & Blueberry oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🥑 Avecado Toast')
+
+streamlite.header('🍌🥭Build Your Own Fruit Smothie🥝🍇')
